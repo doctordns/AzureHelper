e1ecea867ce3de113c323cb1b393b92b2eac8646
unknown <tfl@Cookham8.cookham.net>
2015-05-20 18:47:30 +0100

    More functions

---
+++ Get-azuresubscriptions.ps1
@@ -0,0 +1,27 @@
+﻿
+$Accounts = Get-AzureAccount
+
+foreach ($account in $accounts) 
+{
+  $Subs = $account.subscriptions
+  Foreach ($sub in $subs) 
+  {
+    if ($sub -ne $null) 
+    {
+      $subdetail = Get-AzureSubscription -SubscriptionId $sub
+      if ($subdetail.CurrentStorageAccountName -ne $null)
+      {
+        Select-AzureSubscription -Current -Subscriptionid $subdetail.SubscriptionId 
+        "For Subscription $sub"
+        $accounts = Get-AzureStorageAccount       
+        "  {0} storage accounts" -f $accounts.count
+        foreach ($account in $accounts) 
+        {
+        "     {0,-15}  {1}" -f $account.label,$account.georeplicatedenabled
+      
+        }
+      }
+    }
+  }
+  
+}
