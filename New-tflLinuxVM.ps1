8146d283ef6c3496aaa66443a565b9b989d6a7bf
unknown <tfl@Cookham8.cookham.net>
2015-05-21 11:43:44 +0100

    more updates

---
+++ New-tflLinuxVM.ps1
@@ -0,0 +1,41 @@
+﻿function New-tflLinuxVm {
+
+[CmdletBinding()]
+
+param (
+$Family   = "Ubuntu Server 12.10",
+$Vmname   = "Bunty101",
+$Vmsize   = "Basic_A1",
+$User     = 'tfl',
+$Password = 'Pa$$w0rd',
+$Location = 'West Europe'
+)
+
+# Get image details
+$Image=Get-AzureVMImage | where { $_.ImageFamily -match $Family } | 
+    Sort PublishedDate -Desc |
+       Select -ExpandProperty ImageName -First 1
+If (! $image)
+    { 
+       'No Image Found, returning'
+       return
+    }
+Write-Verbose "Image selected: [$image"]
+
+# Set basic VM details
+$Vm1=New-AzureVMConfig -Name $Vmname -InstanceSize $Vmsize -ImageName  $Image
+
+# Add the creds to the Vm
+$vm1 = $vm1 | Add-AzureProvisioningConfig -Linux -LinuxUser $user -Password $password
+Write-verbose "credentials for [$user/$password] set"
+
+Write-verbose "Creating VM"
+New-AzureVM –ServiceName $vmname -VMs $vm1 -Location 'West Europe'
+
+Get-AzureVM
+
+
+}
+
+# for testing
+New-tflLinuxVm -Family "Ubuntu Server 12.10" -Vmname "Bunty101" -VMsize "Basic_A1" -User 'tfl' -Password 'Pa$$w0rd' -Location 'West Europe' -Verbose
--- Set-tflAzureVMSize.ps1
+++ Set-tflAzureVMSize.ps1
@@ -1,13 +1,32 @@
 ﻿Function Set-tflAzureVMSize {
+<#
+.SYNOPSIS
+    This function changes the VM size of an Azure VM
+.DESCRIPTION
+    This function first checks to see if the Service/VM exists.
+    If so, it changes the size of the Azure VM.
+.NOTES
+    File Name  : Set=tflAzureVMSize
+    Author     : Thomas Lee - tfl@psp.co.uk
+    Requires   : PowerShell Version 3.0
+    Tested     : PowerShell Version 5
+.LINK
+    This script posted to GitHub
+
+.EXAMPLE
+    Set-tflAzureVMSize -VmServicename bunty100 -VmName bunty100 -InstanceSize 'Small'
+    
+#>
 
 [CmdletBinding()]
 Param (
 $VmName        = 'psh1', 
 $VmServicename = 'psh1',
-$VMsize        = 'Basic_A0')
+$InstanceSize  = 'Basic_A0')
 
-# check if VM exists
-$Vm = Get-AzureVM $vmname
+# Check if VM exists
+Write-Verbose "Checking to see if VM [$vmname] in service [$VmServicename] exists"
+$Vm = Get-AzureVM  -Name $vmname -ServiceName $VmServicename
 if (! $vm) 
    {
      "VM $vm Does not exist"; return
@@ -16,23 +35,26 @@ Else
     {
      Write-Verbose "VM exists, continuing"
     }
-# Check if size is valid
-# First get the role sizes
 
+# Check if $InstanceSize size is valid
+# First get the role sizes
 $OkSizes = (Get-AzureLocation).VirtualMachineRoleSizes  | Sort-Object $_ | Select-Object -Unique
 Write-Verbose "$($OkSizes.count) VM sizes available"
-if ( $OkRoles -notcontains $VmSize)
+if ( $OkSizes -notcontains $InstanceSize)
    {
      Write-Verbose "VM Size [$VmSize] is incorrect"
-     Write-Verbose "Valid VM Sizes are as follows:"
      return
    }
+Else {
+     write-Verbose "Vmsisze [$InstanceSize] is valid"
+     }
 
 # Stop if if it's running
 if ($Vm.instanceStatus -eq 'ReadyRole') 
   {
-    Write-Verbose "Stopping VM [$($vm.name)]"
+    Write-Verbose "Stopping VM [$($Vm.name)]"
     $Vm | Stop-AzureVM -force
+    Write-Verbose 'VM Stopped'
     $Vm = Get-AzureVM $vmname
   }
 
@@ -40,7 +62,7 @@ if ($Vm.instanceStatus -eq 'ReadyRole')
  do {
     if ($vm.Status -notmatch 'Stopped') 
       {
-        Write-Verbose "Waiting 15 more seconds"
+        Write-Verbose "Waiting 15 more seconds for shutdown"
         Start-Sleep -Seconds 15}
         $Vm = Get-AzureVM $vmname
       }
@@ -48,18 +70,20 @@ While ($vm.Status -NotMatch 'Stopped')
 
 # So let's change size
 Write-Verbose "VM Stopped - changing size  to $vmSize"
-Set-AzureVMSize -VM $Vm -Instancesize $VmSize   -Verbose |
-     Update-AzureVM -Verbose
-
+Set-AzureVMSize -VM $Vm -Instancesize $InstanceSize  | Update-AzureVM 
 
 # Restart VM
-Write-Verbose 'Size changed, restarting VM'
-Start-AzureVM -Name $vmname -ServiceName $vmservicename
-$Vm = Get-AzureVM $vmname
-Write-Verbose "VM Status: [$($vm.InstanceStatus)]" 
-Write-Verbose 'All done, returning'
+Write-Verbose "Size changed to [$InstanceSize], restarting VM"
+Start-AzureVM -Name $vmname -ServiceName $VmServicename
 
- }
+# Complete
+$Vm = Get-AzureVM $vmname -ServiceName $VmServicename
+$Vm
+Write-Verbose "VM Status: [$($vm.InstanceStatus)]" 
+
+# And return
+Write-Verbose 'All done, returning'
+}
 
  # test it out
  # Change-myAzureVMSize -verbose -VmName psh1 -VmServicename psh1 -VMsize 'ExtraSmall'
