9dcc4aecc6e83d00f17f5f61c0a8f4db6731d0a0
Thomas Lee <tfl@psp.co.uk>
2015-05-25 15:56:19 +0100

    Pre-Watford changes
    Last minute updates - jus adding more code and commiting some work in
    progress

---
+++ AzureHelper/Update-tflAzureHelperModule-todo.ps1
@@ -0,0 +1,22 @@
+﻿$VerbosePreference = 'continue'
+$AzureHelper = Get-Module -Name AzureHelper -ListAvailable
+Write-Verbose "You currently have version $($AzureHelper.Version.ToString()) loaded"
+
+
+# Get the URI
+
+$uri = 'https://api.github.com/repos/doctordns/azurehelper/releases/latest'
+$LatestRelease = Invoke-RestMethod -Uri $uri -Method GET
+
+Write-Verbose "Latest Version on GitHub: [$LatestVersionNumber]"
+$latestrelease
+$LatestVersionNumber = $LatestRelease.name
+
+
+
+
+# Get latest version on GitHub
+$LatestRelease = Invoke-RestMethod -Uri https://api.github.com/repos/Azure/azure-powershell/releases/latest -Method GET
+$LatestVersionNumber = $LatestRelease.name
+Write-Verbose "Latest Version on GitHub: [$LatestVersionNumber]"
+
